08e3a1d22499a327bb89fc4add480527f865647b
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-05-08 19:30:33 -0300

    criando um projeto em React

---
+++ primeiro-projeto/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return <div className="App">
+   <form>
+    <label for='teste'>Nome:</label>
+    <input type="text" id="teste"></input>
+   </form>
+  </div>
+}
+
+export default App;
