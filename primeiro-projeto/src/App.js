c7fc28cbfd2b3793df3c1a1266373c1537d61831
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-05-09 12:41:36 -0300

    conceito de componente e props

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -1,11 +1,18 @@
+import button from "./components/button";
+
+  
 
 function App() {
-  return <div className="App">
-   <form>
-    <label for='teste'>Nome:</label>
-    <input type="text" id="teste"></input>
-   </form>
+  return (
+  <div className="App">
+   <h1>Olá React</h1>
+   <button title="Entrar">Entrar</button>
+   <button title="Sair">Sair</button>
+   <button title="Fechar">Fechar</button>
+   <button title="Enviar">Enviar</button>
   </div>
+  
+  );
 }
 
 export default App;
