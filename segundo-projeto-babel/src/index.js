47a397084e7ea4619d73b04cf56e1591453bd197
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-05-11 13:08:01 -0300

    adicionando react a um projeto

--- segundo-projeto-babel/src/index.js
+++ segundo-projeto-babel/src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import {createRoot} from 'react-dom/client';
 import App from "./app";
+import Button from "./button";
 
 const container = document.getElementById('root')
 const root = createRoot(container);
-root.render(<App/>)
+root.render(<div>
+            <App/> 
+            <Button />
+            </div>)
